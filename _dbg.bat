f1277ae38dc9e346cc927d0b87e5192dabe4ace9
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2022-02-14 17:36:43 +0900

    デバッグスクリプトを更新

--- _dbg.bat
+++ _dbg.bat
@@ -1,9 +1,10 @@
 @echo off
 cd %~dp0
 call _Compile.bat
+move "%~dp0MIRAI.exe" "%~dp0_Debug"
 echo.
 echo プロセス追跡くん経由でアプリケーションを起動
 echo.
 set /P Arg="起動引数を指定: "
 echo.
-Resources\Debug\ProcessTsuisekiKunNN.exe "%~dp0MIRAI.exe" "%Arg%"
+Resources\Debug\ProcessTsuisekiKunNN.exe "%~dp0_Debug\MIRAI.exe" "%Arg%"
